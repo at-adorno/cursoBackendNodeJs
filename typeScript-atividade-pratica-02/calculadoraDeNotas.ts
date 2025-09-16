58ad3f37a9627f4e9b302727fe598cf24ae9329d
atadorno <at.adorno@gmail.com>
2025-09-16 19:23:26 -0300

    correção na calculadoraDeNotas

--- typeScript-atividade-pratica-02/calculadoraDeNotas.ts
+++ typeScript-atividade-pratica-02/calculadoraDeNotas.ts
@@ -1,4 +1,4 @@
-const captura = require("prompt-sync")();
-let n1: number = Number(captura("Nota 1: "));
-let n2: number = Number(captura("Nota 2: "));
+const prompt = require("prompt-sync")();
+let n1: number = Number(prompt("Nota 1: "));
+let n2: number = Number(prompt("Nota 2: "));
 console.log("Média:", (n1 + n2) / 2);
